d909ab285c439b7126dfb3f3d2bb2d86db002bf1
TryUnder <86545780+TryUnder@users.noreply.github.com>
2021-11-18 22:41:13 +0100

    1. added copy constructor 38-42 2.added overload for copy assignment operator 62-68 3.changed default constructor for osoba 33 4. changed overload constructor for osoba 35-36

--- Metody_Programowania/Zad2Klasy.cpp
+++ Metody_Programowania/Zad2Klasy.cpp
@@ -1,104 +1,119 @@
 #include <iostream>
+
 using namespace std;
 
-class adres {
+class adres
+{
 private:
-	string m_miasto, m_ulica;
-	unsigned int m_numer_budynku;
+    string m_miasto, m_ulica;
+    unsigned int m_nr_budynku;
+
 public:
-	adres(string miasto, string ulica, unsigned int numer_budynku) :
-		m_miasto(miasto), m_ulica(ulica), m_numer_budynku(numer_budynku) {};
+    adres() : m_miasto("brak"), m_ulica("brak"), m_nr_budynku(0) {};
 
-	adres() : m_miasto("brak"), m_ulica("brak"), m_numer_budynku(0) {};
+    adres(string miasto, string ulica, unsigned int nr_budynku) :
+        m_miasto(miasto), m_ulica(ulica), m_nr_budynku(nr_budynku) {};
 
-	friend ostream& operator <<(ostream& wypisz, const adres& ob) {
-		wypisz << ob.m_miasto << " " << ob.m_ulica << " " << ob.m_numer_budynku << endl;
-		return wypisz;
-	}
-
-	string& Miasto() {
-		return m_miasto;
-	}
-	string& Ulica() {
-		return m_ulica;
-	}
-	unsigned int& NumerBudynku() {
-		return m_numer_budynku;
-	}
+    friend ostream& operator << (ostream& cout, const adres& obj) {
+        cout << obj.m_miasto << " " << obj.m_ulica << " " << obj.m_nr_budynku;
+        return cout;
+    }
+    string& Miasto() { return m_miasto; }
+    string& Ulica() { return m_ulica; }
+    unsigned int& Nr_budynku() { return m_nr_budynku; }
 };
-
-class osoba {
+class osoba
+{
 private:
-	string m_imie;
-	unsigned int m_wiek;
-	adres* m_adr = nullptr;
+    string m_imie;
+    unsigned int m_wiek;
+    adres* m_adr;
+
 public:
-	osoba(string imie, unsigned int wiek, adres adresx) :
-		m_imie(imie), m_wiek(wiek), m_adr(&adresx) {};
+    osoba() : m_imie("brak"), m_wiek(0), m_adr(new adres) {};
 
-	osoba() :
-		m_imie("brak"),
-		m_wiek(0) {
-		adres* adresx = new adres;
-		m_adr = adresx;
-	};
+    osoba(string const imie, const unsigned int wiek, const adres adr) :
+        m_imie(imie), m_wiek(wiek), m_adr(new adres(adr)) {}
 
-	friend ostream& operator <<(ostream& wypisz, const osoba& obiekt) {
-		wypisz << obiekt.m_imie << " " << obiekt.m_wiek << endl
-			<< obiekt.m_adr->Miasto() << " " << obiekt.m_adr->Ulica() << " "
-			<< obiekt.m_adr->NumerBudynku() << endl;
-		return wypisz;
-	}
+    osoba(osoba& obj) {
+        m_imie = obj.m_imie;
+        m_wiek = obj.m_wiek;
+        m_adr = new adres;
+        *m_adr = *obj.m_adr;
+    }
 
-	string& miasto() {
-		return m_adr->Miasto();
-	}
+    ~osoba() {
+        if (m_adr != nullptr) {
+            delete m_adr;
+            m_adr = nullptr;
+        }
+    }
+
+    friend ostream& operator << (ostream& cout, const osoba& obj) {
+        cout << obj.m_imie << " " << obj.m_wiek << " " << obj.m_adr->Miasto() << " " << obj.m_adr->Ulica()
+            << " " << obj.m_adr->Nr_budynku();
+        return cout;
+    }
+
+    string& miasto() {
+        return m_adr->Miasto();
+    }
+
+    osoba& operator=(const osoba& entity) {
+        if (this != &entity) {
+            this->m_imie = entity.m_imie;
+            this->m_wiek = entity.m_wiek;
+            m_adr = new adres;
+            *m_adr = *entity.m_adr;
+        }
+        return *this;
+    }
 };
 
 int main()
 {
-	adres* wsk = new adres("Czêstochowa", "D¹browskiego", 73);
+    adres* wsk = new adres("Czêstochowa", "D¹browskiego", 73);
 
-	cout << wsk << '\n';
-	cout << *wsk << '\n';
+    cout << wsk << '\n';
+    cout << *wsk << '\n';
 
-	adres a1(*wsk);
+    adres a1(*wsk);
 
-	delete wsk;
-	wsk = nullptr;
+    delete wsk;
+    wsk = nullptr;
 
-	const adres* wsk1 = new adres("£ódŸ", "Piotrkowska", 33);
+    const adres* wsk1 = new adres("£ódŸ", "Piotrkowska", 33);
 
-	cout << a1 << '\n';
-	cout << *wsk1 << '\n';
+    cout << a1 << '\n';
+    cout << *wsk1 << '\n';
 
-	adres a2;
+    adres a2;
 
-	cout << a2 << '\n';
+    cout << a2 << '\n';
 
-	a2 = a1;
+    a2 = a1;
 
-	cout << a2 << '\n';
+    cout << a2 << '\n';
 
-	osoba os1("Ala", 25, *wsk1);
+    osoba os1("Ala", 25, *wsk1);
 
-	delete wsk1;
-	cout << os1 << '\n';
+    delete wsk1;
+    cout << os1 << '\n';
 
-	osoba os2(os1);
+    osoba os2(os1);
 
-	os1.miasto() = "Zmieniono miasto osoby 1.";
+    os1.miasto() = "Zmieniono miasto osoby 1.";
 
-	cout << os1 << endl;
+    cout << os1 << endl;
 
-	cout << os2 << '\n';
+    cout << os2 << '\n';
 
-	osoba os3;
+    osoba os3;
 
-	cout << os3 << '\n';
+    cout << os3 << '\n';
 
-	os3 = os2;
+    os3 = os2;
 
-	os1.miasto() = "Drugi raz zmieniono miasto osoby 1.";
-	cout << os3 << '\n';
+    os1.miasto() = "Drugi raz zmieniono miasto osoby 1.";
+    cout << os3 << '\n';
 }
