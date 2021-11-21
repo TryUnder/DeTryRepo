f8d4ca481700cec0532ee5914a7c3a2ca2e0a978
TryUnder <86545780+TryUnder@users.noreply.github.com>
2021-11-21 23:10:20 +0100

    added operator <<

--- Metody_Programowania/kolo1.cpp
+++ Metody_Programowania/kolo1.cpp
@@ -12,7 +12,9 @@ public:
   K1(const string arg1, const string arg2):
     p1(new string[2] {arg1,arg2}) {};
 
-
+  string fout(const int i) const {
+    return p1[i];
+  }
   K1(string* p):
   p1(new string[2]){};
 
@@ -25,10 +27,18 @@ private:
 public:
   K2(): w1(), w2(0){};
   K2(const string arg1, const string arg2, const double arg3): w1(arg1,arg2), w2(arg3){};
+
+  friend ostream& operator << (ostream& out, const K2& obj){
+    out << obj.w1.fout(0) << " " << obj.w1.fout(1) << " za: " << obj.w2 << endl;
+    return out;
+  }
 };
 
 int main(){
   K2 ob1,ob2;
   const K2* wsk1 = new K2("kawa", " z mlekiem", 4.50);
-//  const K2 ob3(*wsk1);
+  const K2 ob3(*wsk1);
+  cout << ob3 << endl;
+  delete wsk1;
+  wsk1 = 0;
 }
