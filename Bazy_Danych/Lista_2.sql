f1c0e386c5b71c7fee2f6d607c471726ed8790b0
TryUnder <86545780+TryUnder@users.noreply.github.com>
2021-11-28 14:07:22 +0100

    -- up to 2.4

--- Bazy_Danych/Lista_2.sql
+++ Bazy_Danych/Lista_2.sql
@@ -91,3 +91,104 @@ WHERE rok=2
 AND Length(nazwisko) BETWEEN 6 AND 9
 AND Instr(Upper(nazwisko),'A')>0
 ORDER BY Length(nazwisko) DESC;
+
+-- Zad 1.10
+-- Wyświetl listę (patrz Rys. 1.10) zawierającą zmodyfikowane nazwiska i imiona studentek,
+-- których nazwiska rozpoczynają się od liter Ba (bez zastosowania funkcji substr):
+-- • zamiast Ba na początku nazwiska wstaw formułę Start,
+-- • zamiast litery a na końcu imienia staw formułę End.
+
+SELECT nazwisko,
+Concat('Start',Ltrim(nazwisko, 'Ba')) AS "NAZWISKO ZMIENIONE",
+imiona,
+Concat(Rtrim(imiona, 'a'),'End') AS "IMIONA ZMIENIONE"
+FROM studenci
+WHERE Initcap(imiona) LIKE '%a'
+AND nazwisko like ('Ba%');
+
+-- Zad 1.11
+-- Wykorzystując funkcje Rpad() i Lpad() wyświetl nazwiska studentów poprzedzone 3 gwiazdkami (*)
+-- oraz zakończone 4 plusikami (+) (patrz Rys. 1.11).
+
+SELECT Rpad(Lpad(nazwisko,length(nazwisko)+3,'+'),length(nazwisko)+7,'*')
+FROM studenci;
+
+-- Zad 2
+-- Zrealizuj poniższe zadania poprzez utworzenie zapytań zwracających stosowne informacje w oparciu
+-- o aktualne dane przechowywane w tabeli Pojazdy.
+
+-- Zad 2.1
+-- Wyświetl wszystkie pojazdy zarejestrowane w Częstochowie (o numerach rejestracyjnych
+-- rozpoczynających się od SC i posiadających cyfry na ostatnich pozycjach, np. SCZ to powiat częstochowski
+-- a nie miasto; w numerach rejestracyjnych identyfikatory reginalne od pozostałych pozycji oddziela spacja)
+-- o pojemnościach silników mieszczących się w przedziale od 1000 do 2000 cm3 .
+
+SELECT * FROM pojazdy
+WHERE nr_rejestracyjny NOT LIKE('SCZ%')
+AND (nr_rejestracyjny LIKE('SC%0')
+OR nr_rejestracyjny LIKE('SC%1')
+OR nr_rejestracyjny LIKE('SC%2')
+OR nr_rejestracyjny LIKE('SC%3')
+OR nr_rejestracyjny LIKE('SC%4')
+OR nr_rejestracyjny LIKE('SC%5')
+OR nr_rejestracyjny LIKE('SC%6')
+OR nr_rejestracyjny LIKE('SC%7')
+OR nr_rejestracyjny LIKE('SC%8')
+OR nr_rejestracyjny LIKE('SC%9'))
+AND pojemnosc BETWEEN 1000 AND 2000;
+
+-- Zad 2.2
+-- Wyświetl pojazdy marki FORD w kolorze Black Red, w ramach których liczby utworzone przez dwie
+-- ostatnie cyfry z wartości pojemności silnika dzielą się przez liczbę 3 np. 1945 ? 45 / 3 =15 (patrz Rys. 2.2).
+-- Założenie: w zestawieniu należy pominąc pojemności silników „dające” liczby 0 np. 1900.
+
+SELECT pojemnosc FROM pojazdy
+WHERE marka LIKE ('FORD')
+AND kolor LIKE ('Black Red')
+AND Mod(Cast(Substr(To_char(pojemnosc),-2)AS int),3)=0;
+
+-- Zad 2.3
+-- Wyświetl wszystkie motocykle (typ) posiadające w numerach rejestracyjnych przynajmniej dwie cyfry 6
+-- (np. SCZ 1626), których kolory określają przynajmniej dwuczłonowe nazwy (np. Brown Red) a pojemności
+-- silników nie mieszczą się w przedziale od 250 do 500
+
+SELECT *
+FROM pojazdy
+WHERE typ LIKE 'MOTOCYKL'
+AND nr_rejestracyjny LIKE('%6%6%')
+AND kolor LIKE('% %')
+AND pojemnosc NOT BETWEEN 250 AND 500;
+
+-- Zad 2.4
+-- Wyświetl wszystkie pojazdy innego typu niż samochod ciezarowy mające pojemności silnika równe 1000,
+-- 1500 lub 2000 wraz z komentarzami w zakresie oceny tego parametru (Rys. 2.4):
+-- • maly pojazd wobec pojazdów o pojemności 1000,
+-- • sredni pojazd wobec pojazdów o pojemności 1500,
+-- • duzy pojazd wobec pojazdów o pojemności 2000.
+
+SELECT typ,marka,pojemnosc,
+(CASE
+    WHEN pojemnosc=1000 THEN 'MALY_POJAZD'
+    WHEN pojemnosc=1500 THEN 'SREDNI_POJAZD'
+    WHEN pojemnosc=2000 THEN 'DUZY_POJAZD'
+END)AS "KOMENTARZ"
+FROM pojazdy
+WHERE pojemnosc LIKE ('1000')
+OR pojemnosc LIKE ('1500')
+OR pojemnosc LIKE ('2000');
+
+-- Zad 2.5
+-- W oparciu o analizę numerów rejestracyjnych zidentyfikuj pochodzenie pojazdów marki OPEL
+-- o pojemnościach silnia nienależących do przedziału [1600, 2000]. W procesie klasyfikacji pojazdów
+-- uwzględniaj dwie pierwsze litery numerów rejestracyjnych wg następujących zasad (patrz Rys. 2.5):
+-- • SC – slaskie,
+-- • PO – wielkopolskie,
+-- • EL – lodzkie,
+-- • GD – pomorskie,
+-- • pozostałe – niezidentyfikowane.
+
+SELECT Rtrim(nr_rejestracyjny,'WY') FROM pojazdy;
+
+SELECT nr_rejestracyjny, modell, pojemnosc,
+CASE(WHEN  END)AS "WOJEWODZTWO"
+FROM pojazdy;
