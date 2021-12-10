6c66dd3c3c80d1dd2e976662ded92b2d536bb15b
TryUnder <86545780+TryUnder@users.noreply.github.com>
2021-12-10 23:43:57 +0100

    added main

---
+++ Metody_Programowania/Zad7Klasy.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+  double dane[] = { 0.0, 1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8 };
+/*
+  //... zdefiniuj tablicę tab
+
+  tab[0] = new ciezar("cytryny") ;
+  tab[1] = new temp("poranki", dane+3, dane+6);
+  tab[2] = new ciezar("jabłka", dane+1, dane+3) ;
+  tab[3] = new temp("wieczory", dane+1, dane+9);
+  tab[4] = new ciezar;
+
+  cout << "********** 1 **********" << endl;
+  for (int i=0; i<5; ++i)
+    cout << *tab[i] << endl ;
+
+
+  cout << "\n********** 2 **********" << endl;
+
+  for (int i=0; i<5; ++i)
+    try{
+      cout << *tab[i] << ", WYNIK : " << tab[i]->oblicz_wynik() << endl ;
+    }
+    catch (const string& a){
+     cout << a << endl;
+    }
+
+
+  cout << "\n********** 2a *********" << endl;
+  *tab[0] = ciezar("cytryny", dane, dane+1);
+  *tab[0] = ("[kg] " + *tab[0]) += "po wyprzedaży";
+  *tab[1]  = "Wiosenne " + *tab[1];
+  *tab[2] += "ANTONÓWKI suszone";
+  *tab[2] *= 0.1;
+  *tab[3] += "po korekcie odczytu";
+  *tab[3] += 0.1;
+
+  for (int i=0; i<5; ++i)
+    try{
+      cout << *tab[i] << ", WYNIK : " << tab[i]->oblicz_wynik() << endl ;
+    }
+    catch (const string& a){
+     cout << a << endl;
+    }
+
+  for (int i=0; i<5; ++i)
+    delete tab[i];
+
+
+  cout << "\n********** 3 **********" << endl;
+  const ciezar test1("gruszki", dane, dane+9);
+  const temp test2;
+
+  //w odpowiednim miejscu zdefiniuj funkcję pokaz_wszystko(...)
+
+  pokaz_wszystko(cout, test1);
+  pokaz_wszystko(cout, test2);
+  pokaz_wszystko(cout, ciezar());
+  pokaz_wszystko(cout, temp("Jakaś tam", dane+8, dane+9));
+  */
+}
