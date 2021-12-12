55bdc93aeb6eed80f18dd60c623530122c8a455d
TryUnder <86545780+TryUnder@users.noreply.github.com>
2021-12-12 22:01:59 +0100

    update

--- Metody_Programowania/Zad7Klasy.cpp
+++ Metody_Programowania/Zad7Klasy.cpp
@@ -16,13 +16,14 @@ public:
       : m_opis(opis), m_pomiar(new double[ptr_2 - ptr_1 > 0 ? ptr_2-ptr_1 : 0]) {
         size_t size = ptr_2 - ptr_1;
         for(size_t i = 0; i<size; ++i){
-          m_pomiar[i] = *ptr_1;
+          m_pomiar[i] = *(ptr_1);
+          cout << m_pomiar[i] << " ";
           ptr_1++;
         }
         vector <double> m_pomiar_table;
         for(size_t i=0; i<size; ++i){
           m_pomiar_table.push_back(*ptr_1);
-          cout << m_pomiar_table.at(i) << " ";
+          //cout << m_pomiar_table.at(i) << " ";
           ptr_1++;
         }
         cout << endl;
@@ -54,7 +55,7 @@ public:
 class temp : public pomiar{
 private:
 public:
-  temp(const string& opis, double* ptr_1, double* ptr_2)
+  temp(const string& opis, const double* ptr_1, const double* ptr_2)
       : pomiar(opis,ptr_1,ptr_2) {}
 };
 
@@ -63,12 +64,12 @@ int main(){
   //... zdefiniuj tablicę tab
   pomiar* tab[sizeof(dane)/sizeof(dane[0])];
 
-  tab[0] = new ciezar("cytryny") ;
+  tab[0] = new ciezar("cytryny");
   tab[1] = new temp("poranki", dane+3, dane+6);
   tab[2] = new ciezar("jabłka", dane+1, dane+3);
   tab[3] = new temp("wieczory", dane+1, dane+9);
   tab[4] = new ciezar;
-
+  cout << *(dane+3) << endl;
   cout << "********** 1 **********" << endl;
   for (int i=0; i<5; ++i)
     cout << *tab[i] << endl ;
