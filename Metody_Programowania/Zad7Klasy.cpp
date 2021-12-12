3908812779308481195ac063b981a50ccdfe268f
TryUnder <86545780+TryUnder@users.noreply.github.com>
2021-12-12 19:15:15 +0100

    added constructors

--- Metody_Programowania/Zad7Klasy.cpp
+++ Metody_Programowania/Zad7Klasy.cpp
@@ -1,18 +1,55 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-int main(){
+class pomiar{
+private:
+  string m_opis;
+  double* m_pomiar;
+public:
+  pomiar()
+      : m_opis("brak"), m_pomiar(new double(0)) {}
+  pomiar(const string& opis)
+      : m_opis(opis) {}
+  pomiar(const string& opis, const double* ptr_1, const double* ptr_2)
+      : m_opis(opis), m_pomiar(new double[ptr_2 - ptr_1 > 0 ? ptr_2-ptr_1 : 0]) {
+        size_t size = ptr_2 - ptr_1;
+        for(size_t i = 0; i<size; ++i){
+          m_pomiar[i] = *ptr_1;
+          cout << m_pomiar[i] << endl;
+          ptr_1++;
+        }
+      }
+};
 
+class ciezar : public pomiar{
+private:
+public:
+  ciezar(): pomiar() {}
+  ciezar(const string& opis)
+      : pomiar(opis) {}
+  ciezar(const string& opis, const double* ptr_1, const double* ptr_2)
+      : pomiar(opis,ptr_1,ptr_2) {}
+};
+
+class temp : public pomiar{
+private:
+public:
+  temp(const string& opis, double* ptr_1, double* ptr_2)
+      : pomiar(opis,ptr_1,ptr_2) {}
+};
+
+int main(){
   double dane[] = { 0.0, 1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8 };
-/*
   //... zdefiniuj tablicę tab
+  pomiar* tab[sizeof(dane)/sizeof(dane[0])];
 
   tab[0] = new ciezar("cytryny") ;
   tab[1] = new temp("poranki", dane+3, dane+6);
-  tab[2] = new ciezar("jabłka", dane+1, dane+3) ;
+  tab[2] = new ciezar("jabłka", dane+1, dane+3);
   tab[3] = new temp("wieczory", dane+1, dane+9);
   tab[4] = new ciezar;
-
+  /*
   cout << "********** 1 **********" << endl;
   for (int i=0; i<5; ++i)
     cout << *tab[i] << endl ;
