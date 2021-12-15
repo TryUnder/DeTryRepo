e608026e20125c2c7fbad601ae6a3bfea95ec2c2
TryUnder <86545780+TryUnder@users.noreply.github.com>
2021-12-15 21:08:34 +0100

    update

--- Metody_Programowania/Zad7Klasy.cpp
+++ Metody_Programowania/Zad7Klasy.cpp
@@ -3,165 +3,112 @@
 using namespace std;
 #define PURE_VIRTUAL = 0
 
-class pomiar{
+class pomiar {
 protected:
   string m_opis;
+  size_t m_size;
   double* m_pomiar;
 public:
-  pomiar()
-      : m_opis(" \" \" "), m_pomiar(new double(0)) {}
-  pomiar(const string& opis)
-      : m_opis(opis), m_pomiar(new double(0)) {}
-  pomiar(const string& opis, const double* ptr_1, const double* ptr_2)
-      : m_opis(opis), m_pomiar(new double[ptr_2 - ptr_1 > 0 ? ptr_2-ptr_1 : 0]) {
-        size_t size = ptr_2 - ptr_1;
-        for(size_t i = 0; i<size; ++i){
-          m_pomiar[i] = *(ptr_1);
-          //cout << m_pomiar[i] << " ";
-          ptr_1++;
-        }
-      }
-
-  string Get_m_opis() const {
-    return m_opis;
-  }
-
-  double* Get_m_pomiar() const {
-    return m_pomiar;
-  }
-
+  pomiar(): m_opis(" \" \" "), m_size(0), m_pomiar(new double(0)) {}
+  pomiar(const string& opis): m_opis(opis), m_size(0), m_pomiar(new double(0)) {}
+  pomiar(const string& opis, const double* ptr1, const double* ptr2)
+        :m_opis(opis), m_size(ptr2 - ptr1 > 0 ? ptr2 - ptr1 : 0),
+         m_pomiar(new double[m_size]) {
+           for(size_t i=0; i<m_size; ++i){
+             m_pomiar[i] = *ptr1;
+             ptr1++;
+           }
+         }
   virtual string pokaz_opis() const PURE_VIRTUAL;
-  virtual double oblicz_wynik() PURE_VIRTUAL;
-
+  virtual double oblicz_wynik() const PURE_VIRTUAL;
   friend ostream& operator << (ostream& stream, const pomiar& obj){
     stream << obj.pokaz_opis() << obj.m_opis;
     return stream;
   }
-  virtual pomiar& operator=(const pomiar& obj) PURE_VIRTUAL;
+  virtual pomiar& operator = (const pomiar& obj){
+    if(this != &obj){
+      m_opis = obj.m_opis;
+      delete [] m_pomiar;
+      this->m_size = obj.m_size;
+      this->m_pomiar = new double[m_size];
+      *m_pomiar = *obj.m_pomiar;
+        
+
+    }
+    return *this;
+  }
 };
 
-class ciezar : public pomiar{
-private:
+class ciezar : public pomiar {
 public:
   ciezar(): pomiar() {}
-  ciezar(const string& opis)
-      : pomiar(opis) {}
-  ciezar(const string& opis, const double* ptr_1, const double* ptr_2)
-      : pomiar(opis,ptr_1,ptr_2) {}
+  ciezar(const string& opis): pomiar(opis) {}
+  ciezar(const string& opis, const double* ptr1, const double* ptr2)
+       : pomiar(opis,ptr1,ptr2) {}
 
   virtual string pokaz_opis() const override {
     return "Ciezar - ";
   }
-
-  virtual double oblicz_wynik() override {
-    double eps=1e-1;
+  virtual double oblicz_wynik() const override {
     double sum=0;
-    for(int i=0;i<10000000;++i){
-      sum += m_pomiar[i];
-      if(m_pomiar[i]<eps)break;
+      for(size_t i=0; i<m_size; ++i){
+        sum+=m_pomiar[i];
     }
+    //if(sum==0) throw string("brak danych");
     return sum;
   }
-
-  ciezar& operator=(const pomiar& obj) override {
-    if(this != &obj){
-      double eps = 1e-1;
-      this->m_opis = obj.Get_m_opis();
-      delete [] this->m_pomiar;
-      //this->m_pomiar = new double[obj.Get_m_pomiar]
-      //cout << sizeof(obj.Get_m_pomiar())/ << endl;
-      double* m_pomiar2 = obj.Get_m_pomiar();
-      for(int i=0; i<100; ++i){
-        if(*m_pomiar2 < eps){
-          double* True_m_pomiar = new double[i];
-          do{
-            m_pomiar2--;
-            i--;
-            True_m_pomiar[i] = *m_pomiar2;
-            cout << True_m_pomiar[i] << endl;
-          }while(i!=0);
-          break;
-        }
-        m_pomiar2++;
-      }
-    }
-    return *this;
-  }
-
 };
 
-class temp : public pomiar{
-private:
+class temp : public pomiar {
 public:
-  temp(const string& opis, const double* ptr_1, const double* ptr_2)
-      : pomiar(opis,ptr_1,ptr_2) {}
-
+  temp(): pomiar() {}
+  temp(const string& opis): pomiar(opis) {}
+  temp(const string& opis, const double* ptr1, const double* ptr2)
+       : pomiar(opis,ptr1,ptr2) {}
   virtual string pokaz_opis() const override {
     return "Temp - ";
   }
-  virtual double oblicz_wynik() override {
-    double eps=1e-1;
+  virtual double oblicz_wynik() const override {
     double avg=0;
-    for(int i=0;i<10000000;++i){
-      avg += m_pomiar[i];
-      if(m_pomiar[i]<eps){
-        if(i==0)throw string("NIE DZIELIMY PRZEZ ZERO!");
-        avg /= i;
-        break;
-      }
+    for(size_t i=0; i<m_size; ++i){
+      avg+=m_pomiar[i];
     }
-    return avg;
-  }
-
-  temp& operator=(const pomiar&obj) override {
-    if(this != &obj){
-      double eps = 1e-1;
-      this->m_opis = obj.Get_m_opis();
-      delete [] this->m_pomiar;
-      //this->m_pomiar = new double[obj.Get_m_pomiar]
-      //cout << sizeof(obj.Get_m_pomiar())/ << endl;
-      double* m_pomiar2 = obj.Get_m_pomiar();
-      for(int i=0; i<100; ++i){
-        if(*m_pomiar2 < eps){
-          double* True_m_pomiar = new double[i];
-          do{
-            i--;
-            m_pomiar2--;
-            True_m_pomiar[i] = *m_pomiar2;
-            cout << True_m_pomiar[i] << endl;
-          }while(i!=0);
-          break;
-        }
-        m_pomiar2++;
-      }
-    }
-    return *this;
+    if(m_size==0) throw string("NIE DZIEL PRZEZ ZERO");
+    return avg/m_size;
   }
 };
 
+
 int main(){
   double dane[] = { 0.0, 1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8 };
+
   //... zdefiniuj tablicę tab
   pomiar* tab[sizeof(dane)/sizeof(dane[0])];
-  tab[0] = new ciezar("cytryny");
+  tab[0] = new ciezar("cytryny") ;
   tab[1] = new temp("poranki", dane+3, dane+6);
-  tab[2] = new ciezar("jabłka", dane+1, dane+3);
+  tab[2] = new ciezar("jabłka", dane+1, dane+3) ;
   tab[3] = new temp("wieczory", dane+1, dane+9);
   tab[4] = new ciezar;
+
   cout << "********** 1 **********" << endl;
   for (int i=0; i<5; ++i)
-  cout << *tab[i] << endl;
+    cout << *tab[i] << endl ;
+
 
   cout << "\n********** 2 **********" << endl;
 
   for (int i=0; i<5; ++i)
     try{
-      cout << *tab[i] << ", WYNIK : " << tab[i]->oblicz_wynik() << endl;
-    }catch(const string& a){cout << a << endl;}
+      cout << *tab[i] << ", WYNIK : " << tab[i]->oblicz_wynik() << endl ;
+    }
+    catch (const string& a){
+     cout << a << endl;
+    }
+
   cout << "\n********** 2a *********" << endl;
-  cout << *tab[1] << " " << tab[1]->oblicz_wynik() << endl;
-  *tab[2] = ciezar("TEST", dane+3, dane+6);
-  cout << *tab[2] << " " << tab[2]->oblicz_wynik() << endl;
+  cout << *tab[1] << " AVG:" << tab[1]->oblicz_wynik() << endl;
+  *tab[1] = ciezar("kutas", dane+1, dane+9);
+  cout << *tab[1] << " SUM:" << tab[1]->oblicz_wynik() << endl;
   /*
   *tab[0] = ("[kg] " + *tab[0]) += "po wyprzedaży";
   *tab[1]  = "Wiosenne " + *tab[1];
