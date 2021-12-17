78f941d0520997b5d04de9e76f91545347c8d5f5
TryUnder <86545780+TryUnder@users.noreply.github.com>
2021-12-17 17:19:49 +0100

    zad 1

--- Bazy_Danych/Lista_4.sql
+++ Bazy_Danych/Lista_4.sql
@@ -3,4 +3,7 @@
 (od najstarszego) wyprodukowanych w 2018 roku wraz z danymi ich właścicieli (
 */
 
-SELECT * FROM studenci;
+SELECT nr_rejestracyjny, marka, modell, wlasciciel, data_produkcji--Extract(YEAR FROM data_produkcji) as "DATA_PRODUKCJI"
+FROM pojazdy JOIN wlasciciele using(id_wlasciciela)
+WHERE Extract(YEAR FROM data_produkcji)LIKE '2018'
+ORDER BY data_produkcji DESC;
