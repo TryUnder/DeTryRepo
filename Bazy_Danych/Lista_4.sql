51468d1f2e325e74f85e5d6e66a3df277002bea8
TryUnder <86545780+TryUnder@users.noreply.github.com>
2021-12-17 16:14:33 +0100

    added new empty list

---
+++ Bazy_Danych/Lista_4.sql
@@ -0,0 +1,6 @@
+/*
+1. W oparciu o dane zawarte w tabelach Pojazdy i Wlasciciele wyświetl uporządkowaną listę pojazdów
+(od najstarszego) wyprodukowanych w 2018 roku wraz z danymi ich właścicieli (
+*/
+
+SELECT * FROM studenci;
