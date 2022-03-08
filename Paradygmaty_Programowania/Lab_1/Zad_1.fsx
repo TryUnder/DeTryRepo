4ee4313fedc24a2951ab7a4a24d3d6a9028d5ec1
TryUnder <86545780+TryUnder@users.noreply.github.com>
2022-03-08 12:34:58 +0100

    rename

--- Paradygmaty_Programowania/Lab_1/Zad_1.fsx
+++ Paradygmaty_Programowania/Lab_1/Zad_1.fsx
@@ -1,4 +1,4 @@
+  // Zadanie 1.1 Napisz funkcję obliczającą pole koła
   open System;
-  // Zadanie 1.1
   let Field_of_circle(radius) = radius * radius * System.Math.PI;
   System.Console.WriteLine(Field_of_circle(9.0));
