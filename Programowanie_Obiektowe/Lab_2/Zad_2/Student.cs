849af766d8c7552009e707be97f764d18d7ef93b
TryUnder <86545780+TryUnder@users.noreply.github.com>
2022-03-19 12:30:06 +0100

    add

---
+++ Programowanie_Obiektowe/Lab_2/Zad_2/Student.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+namespace Zad_2{
+    public class Student : Person{
+        private int _year;
+        private int _group;
+        private int _indexId;
+        private List<Grade> _grades;
+
+        public int Year { get { return _year; } set { _year = value; }}
+        public int Group { get { return _group; } set { _group = value; }}
+        public int IndexId { get { return _indexId; } set { _indexId = value; }}
+        public List<Grade> Grades { get { return _grades; } set { _grades = value; }}
+        public Student()
+            : base(){
+            _year = 0;
+            _group = 0;
+            _indexId = 0;
+            _grades = new List<Grade>();
+        }
+
+        public Student(string firstName, string lastName, DateTime dateOfBirth, int year, int groupp, int indexId)
+            : base(firstName, lastName, dateOfBirth){
+            _year = year;
+            _group = groupp;
+            _indexId = indexId;
+            _grades = new List<Grade>();
+        }
+
+        public override string ToString(){
+            Console.WriteLine(base.ToString());
+            this.DisplayGrades();
+            return $" Year: {_year} Group: {_group}, IndexId: {_indexId}\n";
+        }
+
+        public void AddGrade(string subjectName, double vvalue, DateTime date){
+            Grade grade = new Grade(subjectName, vvalue, date);
+            _grades.Add(grade);
+        }
+
+        public void AddGrade(Grade grade){
+            _grades.Add(grade);
+        }
+
+        public void DisplayGrades(){
+            foreach(var i in _grades){
+                Console.WriteLine(i);
+            }
+        }
+
+        public void DeleteGrade(string subjectName, double vvalue, DateTime date){
+                Grade test = new Grade(subjectName, vvalue, date);
+                if(_grades.Contains(test)) {Console.WriteLine("YES!");} 
+        }
+
+    }
+}
