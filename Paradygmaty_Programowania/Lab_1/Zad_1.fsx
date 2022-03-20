8ae699bda0f4c8a8bcdaa805e59316b640307d03
unknown <email>
2022-03-20 19:13:44 +0100

    del

--- Paradygmaty_Programowania/Lab_1/Zad_1.fsx
+++
@@ -1,4 +0,0 @@
-  // Zadanie 1.1 Napisz funkcję obliczającą pole koła
-  open System;
-  let Field_of_circle(radius) = radius * radius * System.Math.PI;
-  System.Console.WriteLine(Field_of_circle(9.0));
