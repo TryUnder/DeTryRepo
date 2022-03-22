4a901db92a2b5543ac6cb2d2865419742dd19fc7
unknown <email>
2022-03-22 17:03:49 +0100

    change

--- Programowanie_Obiektowe/Lab_3/Zad_1/Program.cs
+++ Programowanie_Obiektowe/Lab_3/Zad_1/Program.cs
@@ -1,8 +1,9 @@
 using System;
+using System.Linq;
 using System.Collections.Generic;
 namespace Zad_1{
     public class Program{
-        public static void Main(){
+	public static void Main(){
             Item item1 = new Journal(1, "JAISCR", "Springer", new DateTime(2000, 1, 1), 1);            
             Author author = new Author("Robert", "Cook", "Polish");                    
 	    Item item2 = new Book(2, "Agile C#", "Springer", new DateTime(2015, 1, 1), 500, new List<Author>() {author});
@@ -18,14 +19,14 @@ namespace Zad_1{
 	    Catalog catalog = new Catalog("IT C# development", items);
 	    Console.WriteLine(catalog);
 	    string searchedValue = "Agile C#";
-	    Item foundedItemById = catalog.FindItem(item => item.Id == 1);
-	    Item foundedItemByTitle = catalog.FindItem(item => item.Title == searchedValue);
-	    Item foundedItemByDateRange = catalog.FindItem(item => item.DateOfIssue >= new DateTime(2014, 12, 31)
-								&& item.DateOfIssue <= new DateTime(2015, 12, 31));
+	    Item foundedItemById = catalog.FirstOrDefault(item => item.Id == 1);
+	    //Item foundedItemByTitle = catalog.FindItem(item => item.Title == searchedValue);
+	    //Item foundedItemByDateRange = catalog.FindItem(item => item.DateOfIssue >= new DateTime(2014, 12, 31)
+		//						&& item.DateOfIssue <= new DateTime(2015, 12, 31));
 	    Console.WriteLine("++++++++++++++++++++++++++++++++++");
 	    Console.WriteLine(foundedItemById);
-	    Console.WriteLine(foundedItemByTitle);
-	    Console.WriteLine(foundedItemByDateRange);
+	    //Console.WriteLine(foundedItemByTitle);
+	    //Console.WriteLine(foundedItemByDateRange);
 	}
     }
 }
