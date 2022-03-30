9c258c50b3334ce46ba1664a35bcfd74f6a63a62
DeTry <sniacyinaczej@gmail.com>
2022-03-30 21:11:06 +0200

    x

--- Programowanie_Niskopoziomowe/Lab_4/Zad_7_x64.cpp
+++ Programowanie_Niskopoziomowe/Lab_4/Zad_7_x64.cpp
@@ -7,8 +7,8 @@ extern "C" void Zad_7_x64(long long int _size, long long int* _ptr);
 int main() {
 	random_device rd;
 	mt19937 gen(rd());
-	uniform_int_distribution<> distrib(1, 100);
-	long long int _size = 5;
+	uniform_int_distribution<> distrib(1, 2000);
+	long long int _size = distrib(gen);
 	long long int* _arr = new long long int[_size];
 	Zad_7_x64(_size,_arr);
 	for (int i = 0; i < _size; ++i) {
