3fdbeddae2b015037912ccfb0a2475f1e5ab7bae
DeTry <sniacyinaczej@gmail.com>
2022-04-15 18:05:46 +0200

    fix

--- University/Script_Languages/Powershell/Powershell3.ps1
+++ University/Script_Languages/Powershell/Powershell3.ps1
@@ -24,14 +24,31 @@ należy włączyć możliwość wyboru koloru.
 
 Add-Type -AssemblyName System.Windows.Forms
 
+function LoadFont{
+    if($TNR.Checked -EQ $true){
+        if($italics.Checked -EQ $true){
+            $result.Font = New-Object System.Drawing.Font("Times New Roman",18,[System.Drawing.FontStyle]::Italic)
+        }else{
+            $result.Font = New-Object System.Drawing.Font("Times New Roman",18)
+        }
+    }
+    if($Courier.Checked -EQ $true){
+        if($italics.Checked -EQ $true){
+            $result.Font = New-Object System.Drawing.Font("Courier",18,[System.Drawing.FontStyle]::Italic)
+        }else{
+            $result.Font = New-Object System.Drawing.Font("Courier",18)
+        }
+    }
+}
+
 <# 1.1 . 1.2 . 1.3 . 1.4 #>
 <# Main window #>
-$window = New-Object System.Windows.Forms.Form
-$window.Text = "Laboratorium 3.1"
-$window.Width = 800
-$window.Height = 600
-$window.MinimumSize = New-Object System.Drawing.Size(800,600)
-$window.MaximumSize = New-Object System.Drawing.Size(800,600)
+$windowMain = New-Object System.Windows.Forms.Form
+$windowMain.Text = "Laboratorium 3.1"
+$windowMain.Width = 800
+$windowMain.Height = 600
+$windowMain.MinimumSize = New-Object System.Drawing.Size(800,600)
+$windowMain.MaximumSize = New-Object System.Drawing.Size(800,600)
 
 <# 1.5 #>
 <# Text box 1 #>
@@ -89,24 +106,96 @@ $buttonDiv.Add_Click({
     }
 })
 
-<# 1.7 #>
+<# 1.7 . 1.8 . 1.13 #>
 <# Button Color #>
+$buttonCol = New-Object System.Windows.Forms.Button
+$buttonCol.Location = New-Object System.Drawing.Size(100,200)
+$buttonCol.Width = 150
+$buttonCol.Text = "Wybierz kolor"
+$buttonCol.Add_Click({
+    $colorChoose = New-Object System.Windows.Forms.ColorDialog
+    $colorChoose.ShowDialog()
+    $result.ForeColor = $colorChoose.Color
+})
 
+
+<# 1.11 . 1.12 . 1.13 #>
+$buttonFont = New-Object System.Windows.Forms.Button
+$buttonFont.Location = New-Object System.Drawing.Size(250,200)
+$buttonFont.Width = 150
+$buttonFont.Text = "Wybierz czcionkę"
+$buttonFont.Add_Click({
+
+    <# Color #>
+    $buttonCol2 = New-Object System.Windows.Forms.Button
+    $buttonCol2.Location = New-Object System.Drawing.Size(20,140)
+    $buttonCol2.Width = 155
+    $buttonCol2.Text = "Wybierz kolor"
+    $buttonCol2.Add_Click({
+        $colorChoose2 = New-Object System.Windows.Forms.ColorDialog
+        $colorChoose2.ShowDialog()
+        $result.ForeColor = $colorChoose2.Color
+    })
+
+    $windowFont = New-Object System.Windows.Forms.Form
+    $windowFont.Width = 400
+    $windowFont.Height = 300
+
+    <# GroupBox #>
+    $groupBox = New-Object System.Windows.Forms.GroupBox
+    $groupBox.Location = New-Object System.Drawing.Size(20,20)
+    $groupBox.Width = 155
+
+    <# Italics #>
+    $italics = New-Object System.Windows.Forms.CheckBox
+    $italics.Location = New-Object System.Drawing.Size(20,180)
+    $italics.Text = "Kursywa"
+    $italics.Width = 150
+
+    <# TNR #>
+    $TNR = New-Object System.Windows.Forms.RadioButton
+    $TNR.Location = New-Object System.Drawing.Size(15,20)
+    $TNR.Width = 120
+    $TNR.Height = 30
+    $TNR.Text = "Times New Roman"
+    $TNR.Checked = $true
+
+    <# Courier #>
+    $Courier = New-Object System.Windows.Forms.RadioButton
+    $Courier.Location = New-Object System.Drawing.Size(15,60)
+    $Courier.Width = 120
+    $Courier.Height = 30
+    $Courier.Text = "Courier"
+    $Courier.Checked = $false
+
+    $TNR.Add_Click({LoadFont})
+    $Courier.Add_Click({LoadFont})
+    $groupBox.Controls.Add($TNR)
+    $groupBox.Controls.Add($Courier)
+    $windowFont.Controls.Add($buttonCol2)
+    $windowFont.Controls.Add($groupBox)
+    $windowFont.Controls.Add($italics)
+    $windowFont.ShowDialog()
+})
 
 <# 1.6 #>
 <# Label Result #>
 $result = New-Object System.Windows.Forms.Label
-$result.Location = New-Object System.Drawing.Size(365,150)
-$result.Width = 70
-[float]$result.Text = 0
+$result.Location = New-Object System.Drawing.Size(350,150)
+$result.Width = 100
+[float]$result.Text = 0.0
 $result.TextAlign = "MiddleCenter"
 
 <# Controls #>
-$window.Controls.Add($textBox1)
-$window.Controls.Add($textBox2)
-$window.Controls.Add($buttonAdd)
-$window.Controls.Add($buttonSub)
-$window.Controls.Add($buttonMul)
-$window.Controls.Add($buttonDiv)
-$window.Controls.Add($result)
-$window.ShowDialog()
+$windowMain.Controls.Add($textBox1)
+$windowMain.Controls.Add($textBox2)
+$windowMain.Controls.Add($buttonAdd)
+$windowMain.Controls.Add($buttonSub)
+$windowMain.Controls.Add($buttonMul)
+$windowMain.Controls.Add($buttonDiv)
+$windowMain.Controls.Add($buttonCol)
+$windowMain.Controls.Add($buttonFont)
+$windowMain.Controls.Add($result)
+$windowMain.ShowDialog()
+
+
