f6f44226d316a372094dbc8579c9bb6caa24e03f
DeTry <sniacyinaczej@gmail.com>
2022-04-15 19:26:31 +0200

    add

---
+++ University/Script_Languages/Powershell/Powershell3_2.ps1
@@ -0,0 +1,46 @@
+<#
+Zadanie 2:
+2.1 Utwórz nowy skrypt.
+2.2 Utwórz w nim okno programu o wymiarach 640x480.
+2.3 Ustaw tytuł okna na Laboratorium 3.2.
+2.4 Wewnątrz okna stwórz przycisk Wybierz, który wywoła okno dialogowe wyboru folderu.
+2.5 Utwórz pole tekstowe.
+2.6 Utwórz kolejny przycisk. Nazwij go Utwórz. Po naciśnięciu klawisza ma się utworzyć
+folder o nazwie pobranej z pola tekstowego w lokalizacji pobranej z okna dialogowego
+wyboru katalogu.
+2.7 Dodaj okno zaznaczenia o nazwie Ukryty. Każdorazowo po zaznaczeniu/odznaczeniu go
+ma następować zmiana atrybutu ukryty utworzonego wcześniej folderu.
+#>
+
+Add-Type -AssemblyName System.Windows.Forms
+
+<# Main Window #>
+$window = New-Object System.Windows.Forms.Form
+$window.Location = New-Object System.Drawing.Size(640,480)
+$window.MinimumSize = New-Object System.Drawing.Size(640,480)
+$window.MaximumSize = New-Object System.Drawing.Size(640,480)
+$window.Text = "Laboratorium 3.2"
+
+<# Button #>
+$buttonFolder = New-Object System.Windows.Forms.Button
+$buttonFolder.Location = New-Object System.Drawing.Size(320,72)
+$buttonFolder.Width = 128
+$buttonFolder.Height = 24
+$buttonFolder.Text = "Wybierz..."
+$buttonFolder.Add_Click({
+    $openFileDialog = New-Object System.Windows.Forms.FolderBrowserDialog
+    $openFileDialog.ShowDialog()
+    Write-Host $openFileDialog.SelectedPath
+})
+<# Folder Label #>
+$label = New-Object System.Windows.Forms.Label
+$label.Location = New-Object System.Drawing.Size(160, 75)
+$label.Width = 128
+$label.Height = 24
+$label.Text = "Wybierz folder: "
+$label.Font = New-Object System.Drawing.Font("Times New Roman",12, [System.Drawing.FontStyle]::Bold)
+
+<# Load #>
+$window.Controls.Add($buttonFolder)
+$window.Controls.Add($label)
+$window.ShowDialog()
