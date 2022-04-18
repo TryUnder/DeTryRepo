3a190874be538a43bfc9e1e3bb1228b699d224c4
DeTry <sniacyinaczej@gmail.com>
2022-04-18 17:16:42 +0200

    new class

--- University/Object_Oriented_Programming/Lab_4/Assignments/Assignment_1/Program.cs
+++ University/Object_Oriented_Programming/Lab_4/Assignments/Assignment_1/Program.cs
@@ -2,12 +2,25 @@ using System;
 namespace Assignment_1{
     public class Program{
         public static void Main(){
+            Console.WriteLine("************************************************************************");
             Student student1 = new Student("Jan","Kowalski", new DateTime(1995, 1, 1), "Informatyka", 1);
-            Console.WriteLine(student1);
             Student student2 = new Student("Piotr","Nowak", new DateTime(1990, 1, 1), "Matematyka", 3, 2);
-            Console.WriteLine(student2);
             Person student3 = new Student("Adam", "Bednarski", new DateTime(1993, 1, 1), "Informatyka", 1, 2);
+            Console.WriteLine(student1);
+            Console.WriteLine(student2);
             Console.WriteLine(student3);
+
+            Console.WriteLine("************************************************************************");
+            Subject subject1 = new Subject("Programowanie obiektowe", "Informatyka", 4, 30);
+            Subject subject2 = new Subject("Bazy danych", "Informatyka", 4, 30);
+            Subject subject3 = new Subject("Algebra", "Matematyka", 1, 15);
+            Subject subject4 = new Subject("Analiza", "Matematyka", 1, 30);
+            Console.WriteLine(subject1);
+            Console.WriteLine(subject2);
+            Console.WriteLine(subject3);
+            Console.WriteLine(subject4);
+
+            Console.WriteLine("************************************************************************");
         }
     }
 }
---
+++ University/Object_Oriented_Programming/Lab_4/Assignments/Assignment_1/Subject.cs
@@ -0,0 +1,20 @@
+using System;
+namespace Assignment_1{
+    public class Subject{
+        public string Name { get; set; }
+        public string Specialization { get; set; }
+        public int Semester { get; set; }
+        public int HoursCount { get; set;}
+        public Subject(string name, string specialization, int semester, int hoursCount){
+            Name = name;
+            Specialization = specialization;
+            Semester = semester;
+            HoursCount = hoursCount;
+        }
+
+        public override string ToString(){
+            string subject = $"Name: {Name}, Specialization: {Specialization}, Semester: {Semester}, HoursCount: {HoursCount}";
+            return subject;
+        }
+    }
+}
