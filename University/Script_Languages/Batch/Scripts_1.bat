18488625ceb99e6eb312bd7c5f6bf8e47903dc05
DeTry <sniacyinaczej@gmail.com>
2022-04-19 16:41:52 +0200

    update

--- University/Script_Languages/Batch/Scripts_1.bat
+++ University/Script_Languages/Batch/Scripts_1.bat
@@ -20,6 +20,61 @@ PAUSE
 :: Wyświetlić informacje o przedstawionych powyżej komendach. Wywołać komendę dir w taki sposób aby
 :: wyświetlała za pomocą małych liter tylko nazwy plików i folderów ukrytych z katalogu C:\Windows.
 
+:: help (commands)
+
+:: TIME - Is used to display information about the current time as to change it
+:: /T   - with the only purpose for displaying
+
+:: DATE - Is used to display information about the current date as to change it
+:: /T   - with the only purpose for displaying
+
+:: EXIT - Quits the command interpreter or the current batch script
+:: /B   - Inside the script, exits from script. Outside of the script, will quit CMD.EXE
+
+:: VOL  - Display information about specific Volumes
+
+:: chdir (cd) - 
+::  1)Without any parameters CHDIR displays the current drive and directory
+::  2)In order to change the drive /D is needed
+
+:: COPY - Copies one or more files to another location.
+:: COPY source destination
+
+:: CLS - Clears the screen
+
+:: DEL - Deletes one or more files
+::  1) /P Prompt for confirmation
+::  2) /F Force delete read-only files
+::  3) /S Delete specified files from all subdirectories.
+::  4) /Q Quiet mode
+::  5) /A Select files to delete based on attributes
+::      /A[:D/H/S/R]
+
+:: MKDIR (MD) - Creates a directory
+
+:: RENAME (REN) - This command is responsible for changing name of the files
+:: RENAME BEFORE-CHANGE-NAME AFTER-CHANGE-NAME
+
+:: RMDIR (RD) - Used to remove directories
+::  1) /S Removes all directories and files in the specified directory
+::  2) /Q Quiet mode
+
+:: TYPE - Displays the contents of a text file or files
+
+:: TITLE - Change title of the window console command 
+
+:: PROMPT - Is used to change the command prompt
+
+:: FINDSTR - Searches for strings in files
+::  1) /S Searches for matching files in the current directory and all subdirectories
+::  2) /I Case sensitivity disabled
+::  3) /R Regular expression ENABLED
+
+:: ATTRIB - Displays or change file attributes
+::  1) + Sets an attribute
+::  2) - Clears an attribute
+::  R/A/S/H
+
 :: DIR - Displays a list of files and subdirectories in a directory
 :: Switch
 :: /A Displays files with specified attributes.
@@ -84,4 +139,28 @@ echo %RANDOM%
 :: Jeżeli dany katalog istnieje, zmienić jego nazwę na nazwa.old (lub zapytać użytkownika co zrobić – np. czy
 :: usunąć dany katalog). Przykład wczytania danych od użytkownika: set /P katalog="Podaj nazwe katalogu: "
 
+
+:START_
+SETLOCAL
 set /P readLine="Podaj nazwe katalogu: "
+mkdir %TEMP%\%readLine%
+if %ERRORLEVEL%==0 (
+    echo Directory was created succesfully
+) else (
+    echo Directory was already created
+    set /P readLine2="Do you want to delete that directory?: "
+    if %readLine2% ==Y (
+        rd %TEMP%\%readLine%
+        echo Directory was deleted succesfully
+        set /P readLine3="Woud you like to create a directory?: "
+        if %readLine3% ==Y (
+            ENDLOCAL
+            goto START_
+        ) else (
+            echo exit from the program
+        )
+    ) else (
+        echo Exit from the program
+    )
+)
+::DOESNT WORK
