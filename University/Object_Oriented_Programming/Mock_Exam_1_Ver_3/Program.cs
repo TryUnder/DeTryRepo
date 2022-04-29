9d4611979686ce230070f1b1be939252b15b0d94
DeTry <sniacyinaczej@gmail.com>
2022-04-29 11:48:32 +0200

    x

--- University/Object_Oriented_Programming/Mock_Exam_1_Ver_3/Program.cs
+++ University/Object_Oriented_Programming/Mock_Exam_1_Ver_3/Program.cs
@@ -5,7 +5,7 @@ namespace Mock_Exam_1_Ver_3{
         public static void Main(){
             Towar towar1 = new Napoje("Fanta", 5.00f, 1.5f);
             Console.WriteLine(towar1);
-            Console.WriteLine(towar1.)
+            //Console.WriteLine(towar1.)
 
             Towar towar2 = new Owoce("Jabłka", 2.50f, 2015, 1.25f);
             Console.WriteLine(towar2);
