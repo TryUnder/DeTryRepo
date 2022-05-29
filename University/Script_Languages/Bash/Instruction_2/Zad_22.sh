ba29db6585717afa4827025f862b464f36ef3955
DeTry <sniacyinaczej@gmail.com>
2022-05-29 22:50:10 +0200

    update

--- University/Script_Languages/Bash/Instruction_2/Zad_22.sh
+++ University/Script_Languages/Bash/Instruction_2/Zad_22.sh
@@ -1,4 +1,4 @@
 # 22. Zapisz do pliku zawartosc katalogu /usr/doc. Do tego samego pliku dolacz zawartosc
 # katalogu /usr/local. Zobacz plik z wynikami.
 
-/usr/doc > $HOME/Zad_22; /usr/local >> $HOME/Zad_22
+ll /usr/doc > $HOME/Zad_22; ll /usr/local >> $HOME/Zad_22
