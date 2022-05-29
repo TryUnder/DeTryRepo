e6a95eaadd0cda524b36b6f32a4d5e88f432515b
DeTry <sniacyinaczej@gmail.com>
2022-05-29 23:23:09 +0200

    some info for colq

--- University/Script_Languages/Bash/Instruction_5/Zad_7.sh
+++ University/Script_Languages/Bash/Instruction_5/Zad_7.sh
@@ -1,3 +1,15 @@
 # https://knowledge.broadcom.com/external/article/28212/ftp-ascii-vs-binary-mode-what-it-means.html
 # TXT -> ASCII
 # rest -> BIN
+
+# touch pozwala przyjmować kilka parametrów w postaci nowych plików do tworzenia w danym katalogu
+# wszystkie parametry programu mv poza ostatnim określają pliki które mamy przenieść lub którym nazwę chcemy zmienić
+# komenda cp/mv działa z *
+# samo rm nie usunie katalogów (do tego służy rmdir) (lub -r - usuwa rekursywnie katalogi) i plików ukrytych (-f)
+# ln -s (symbolic) source, nazwa dowiązania
+# ls -i (nodes id)
+# do zakonczenia procesu o danym  PID służy poleceni kill
+# dialog --msgbox 
+# dialog --yesno
+# dialog --inputbox
+# dialog --menu
