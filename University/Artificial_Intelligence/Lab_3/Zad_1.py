41d14ae4881bc4131cd1522773e4967b4a4582ec
DeTry <sniacyinaczej@gmail.com>
2022-11-07 21:16:26 +0100

    add

--- University/Artificial_Intelligence/Lab_3/Zad_1.py
+++ University/Artificial_Intelligence/Lab_3/Zad_1.py
@@ -14,25 +14,26 @@ class Perceptron:
         self.givenValue = []
         
     def ActivationFunction(self):
-        for i in range(len(self.xVector)):
-            self.sigma.insert(i,self.xVector[i][0] * self.xWeight[1] + self.xVector[i][1] * self.xWeight[2] + self.xWeight[0] * self.thresholdValue)
+        for i in range(0,len(self.xVector),1):
+            self.sigma.append(self.xVector[i][0] * self.xWeight[1] + self.xVector[i][1] * self.xWeight[2] + self.xWeight[0] * self.thresholdValue)
             if self.sigma[i] >= 0:
-                self.givenValue.insert(i,1)
+                self.givenValue.append(1)
             else:
-                self.givenValue.insert(i,0)
-            #self.error.insert(i,self.expectedValue[i] - self.givenValue[i])
+                self.givenValue.append(0)
+            #self.error.append(self.expectedValue[i] - self.givenValue[i])
 
     def CorrectWeights(self):
+        self.ActivationFunction()
         self.PlotFigure(parameter3 = "Prosta przed nauka")
-        for i in range(self.iters):
+        for i in range(0,self.iters,1):
             self.ActivationFunction()
             counter = 0
             for j in range(0,len(self.xVector),1):
-                self.error.insert(j,self.expectedValue[j] - self.givenValue[j])
+                self.error.append(self.expectedValue[j] - self.givenValue[j])
                 self.xWeight[0] = self.xWeight[0] + self.learningRate * self.error[j] * self.thresholdValue
                 self.xWeight[1] = self.xWeight[1] + self.learningRate * self.error[j] * self.xVector[j][0]
                 self.xWeight[2] = self.xWeight[2] + self.learningRate * self.error[j] * self.xVector[j][1]
-            for k in range(len(self.error)):
+            for k in range(0,len(self.error),1):
                 if self.error[k] == 0:
                     counter += 1
                 if counter == len(self.error):
@@ -43,32 +44,30 @@ class Perceptron:
             self.error.clear()
             self.sigma.clear()
             self.givenValue.clear()
- 
 
     def PlotFigure(self, parameter3):
-        xGenerator = np.arange(-10,10)
-        line = -(self.xWeight[1] / self.xWeight[2]) * xGenerator + (self.xWeight[0] / self.xWeight[2])
-        plt.plot(xGenerator, line, 'r-', label = parameter3)
+        #xGenerator = np.arange(-10,10)
+        #self.TestFunction()
+        #print(len(self.xVector))
+        line = -(self.xWeight[1] / self.xWeight[2]) * self.xVector[:,0] + (self.xWeight[0] / self.xWeight[2])
+        plt.plot(self.xVector[:,0], line, 'r-', label = parameter3)
         plt.legend()
-        for i in range(len(self.xVector)):
+        for i in range(0,len(self.xVector),1):
             if self.expectedValue[i] == 0:
-                if self.xVector[i,1] > -(self.xWeight[1] / self.xWeight[2]) * self.xVector[i,0] + (self.xWeight[0] / self.xWeight[2]):
-                    plt.plot(self.xVector[i,0], self.xVector[i,0], "ro")
+                if self.xVector[i,1] > line[i]:
+                    plt.plot(self.xVector[i,0], self.xVector[i,1], "bo")
                 else:
                     plt.plot(self.xVector[i,0], self.xVector[i,1], "go")
             else:
-                if self.xVector[i,1] < -(self.xWeight[1] / self.xWeight[2]) * self.xVector[i,0] + (self.xWeight[0] / self.xWeight[2]):
-                    plt.plot(self.xVector[i,0], self.xVector[i,0], "ro")
+                if self.xVector[i,1] < line[i]:
+                    plt.plot(self.xVector[i,0], self.xVector[i,1], "ro")
                 else:
                     plt.plot(self.xVector[i,0], self.xVector[i,1], "m*")
         plt.show()
 
     def TestFunction(self):
-        for i in range (len(self.xVector)):
-            print("x1: ", self.xVector[i][0], " x2: ", self.xVector[i][1], " oczekiwany y: ", self.expectedValue[i], " obliczony: ", self.givenValue[i], " error: ", self.error[i])
-
-
-            
+        for i in range(len(self.xVector)):
+            print("x1: ", self.xVector[i][0], " x2: ", self.xVector[i][1], " oczekiwany y: ", self.expectedValue[i])
             
 perceptron = Perceptron(np.array([[-4,3],[-2,3],[-1,2],[1,2],[2,2],[2,1],[1,4],[1,5],[2,4],[4,4],
                          [4,3],[4,2],[4,1],[8,7],[6,1],[6,3],[6,5],[3,6],[5,6],[-5,7],
@@ -78,6 +77,6 @@ perceptron = Perceptron(np.array([[-4,3],[-2,3],[-1,2],[1,2],[2,2],[2,1],[1,4],[
                                    1,1,1,1,1,1,1,1,1,1,
                                    0,0,0,0,0,0,0,0,0,0,
                                    0,0,0,0,0,0,0,0,0,0]),
-                         [np.random.random(1)-1, np.random.random(1)-1, np.random.random(1)-1], -1.0, 100, 0.02)
+                         [np.random.random(1)-0.5, np.random.random(1)-0.25, np.random.random(1)-1], -1.0, 100, 0.01)
 
 perceptron.CorrectWeights()
