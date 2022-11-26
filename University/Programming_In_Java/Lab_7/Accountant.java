2e38a121f1e222cef2f7101b399b55616ccc1efb
DeTry <sniacyinaczej@gmail.com>
2022-11-26 20:30:00 +0100

    xx

--- University/Programming_In_Java/Lab_7/Accountant.java
+++ University/Programming_In_Java/Lab_7/Accountant.java
@@ -4,5 +4,5 @@ public interface Accountant {
 	public static final double ratePIT = 0.18;
 	public static final double hcc = 0.09;
 	
-	public void countSalary();
+	public abstract void countSalary(double netSalary);
 }
