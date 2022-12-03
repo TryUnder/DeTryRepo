9ef9dbd6fb88f98cb29357497dd59f33ad3cc96c
DeTry <sniacyinaczej@gmail.com>
2022-12-03 01:43:19 +0100

    add

--- University/Programming_In_Java/Lab_8/main.java
+++ University/Programming_In_Java/Lab_8/main.java
@@ -7,17 +7,20 @@ public class main {
         System.out.println(board);
         Board.Point truePoint = board.new Point(1,2);
 
-        Board.Point userPoint = board.new Point();
+        //Board.Point userPoint = board.new Point();
         
         // userPoint.setX(new Scanner(System.in).nextInt());
         // userPoint.setY(new Scanner(System.in).nextInt());
         Pair<Integer, Integer> userPointPair = new Pair<Integer, Integer>();
         ArrayList<Pair<Integer, Integer>> userPointPairList = new ArrayList<Pair<Integer, Integer>>();
+        
+        ArrayList<Board.Point> userPoint = new ArrayList<Board.Point>();
         for (int i = 0; i < 2; ++i) {
+            userPoint.add(board.new Point(new Scanner(System.in).nextInt(), new Scanner(System.in).nextInt()));
             //userPoint.setX(new Scanner(System.in).nextInt());
             //userPoint.setY(new Scanner(System.in).nextInt());
-            //userPointPair.add(userPoint.getX(), userPoint.getY());
-            //userPointPairList.add(userPointPair);
+            userPointPair.add(userPoint.get(i).getX(), userPoint.get(i).getY());
+            userPointPairList.add(userPointPair);
         }
         System.out.println(userPointPairList.get(0).getFst().toString() + userPointPairList.get(0).getSnd().toString());
         System.out.println(userPointPairList.get(1).getFst().toString() + userPointPairList.get(1).getSnd().toString());
