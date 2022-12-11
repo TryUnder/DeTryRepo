3a7263a011b0c0104a4a98e8526e0fad9bd86e93
DeTry <sniacyinaczej@gmail.com>
2022-12-11 15:47:16 +0100

    updated algorithm without sysouts

--- University/Programming_In_Java/Lab_9/Player.java
+++ University/Programming_In_Java/Lab_9/Player.java
@@ -21,22 +21,11 @@ public class Player {
         for (int i = 0; i < hand.size(); ++i) {
             for (int j = 0; j < deletedPairs.size(); ++j) {
                 if (hand.get(i).compare(deletedPairs.get(j)) == true) {
-                    //System.out.println("Del ins: " + deletedPairs.get(j));
                     t++;
                     if (t == deletedPairs.size() + 1){
                         while(hand.get(i).compare(deletedPairs.get(j)) == true) {
-                            System.out.println("Zawiera!: z hand: " + hand.get(i) + " z deletedPair: " + deletedPairs.get(j));
-                            System.out.print("Hand przed usunieciem: ");
-                            hand.forEach(element -> System.out.print(element + " "));
-                            System.out.println();
                             hand.removeLast();
-                            System.out.print("Hand po usunieciu: ");
-                            hand.forEach(element -> System.out.print(element + " "));
-                            System.out.println();
                             hand.add(new Pair(pack.get(new Random().nextInt(rand1)).getFst(), pack.get(new Random().nextInt(rand2)).getSnd()));
-                            System.out.println("Hand po dodaniu: ");
-                            hand.forEach(element -> System.out.println(element + " "));
-                            System.out.println();
                         }
                     }
                 }
