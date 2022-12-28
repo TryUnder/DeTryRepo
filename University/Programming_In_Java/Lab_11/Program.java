69965ee36c572ac145fc28b1c36e0d61b9148cf4
DeTry <sniacyinaczej@gmail.com>
2022-12-28 16:26:49 +0100

    update 2.1

--- University/Programming_In_Java/Lab_11/Program.java
+++ University/Programming_In_Java/Lab_11/Program.java
@@ -1,12 +1,17 @@
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.Map;
 import java.util.List;
+import java.util.LongSummaryStatistics;
 import java.util.TreeMap;
+import java.util.function.Function;
+import java.util.IntSummaryStatistics;
 import java.util.stream.Collectors;
 import java.util.regex.Pattern;
+import java.util.stream.*;
 
 public class Program {
     public static void main(String[] args) throws IOException {
@@ -17,6 +22,9 @@ public class Program {
             .collect(Collectors.groupingBy(String::toUpperCase,
             TreeMap::new, Collectors.counting()));
 
+        String pureText = new String(Files.readAllBytes(Paths.get("BajkaCK.txt")), StandardCharsets.UTF_8);
+        List<String> pureTextList = List.of(pureText.split("\\PL+"));
+
         wordCounts.entrySet().stream().collect(
             Collectors.groupingBy(entry -> entry.getKey().charAt(0), TreeMap::new,
             Collectors.toList())).forEach((key, value) -> {
@@ -24,6 +32,10 @@ public class Program {
                 value.stream().forEach(word -> System.out.printf("%s (%d) ", word.getKey(), word.getValue()));
             });
         
-            
-    }    
+            //IntSummaryStatistics summary = pureTextList.stream().mapToInt(e -> Integer.parseInt(e)).summaryStatistics();
+            //System.out.println(summary.getCount());
+
+            LongSummaryStatistics stats = wordCounts.entrySet().stream().mapToLong(x -> x.getValue()).summaryStatistics();
+            System.out.printf("\n\nLaczna liczba slow: %d Liczba roznych slow: %f", stats.getSum(), stats.getAverage());
+        }    
 }
