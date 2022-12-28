076d646cb4bea379c5d63009ba2a1f3c878c4802
DeTry <sniacyinaczej@gmail.com>
2022-12-28 21:02:58 +0100

    update

--- University/Programming_In_Java/Lab_11/Program.java
+++ University/Programming_In_Java/Lab_11/Program.java
@@ -14,8 +14,15 @@ import java.util.stream.Collectors;
 import java.util.regex.Pattern;
 import java.util.stream.*;
 import java.util.function.Function;
+import java.util.Scanner;
+import java.util.Optional;
 
 public class Program {
+
+    public static Optional<Long> check(Long value) {
+        return value == 0 ? Optional.empty() : Optional.of(value);
+    }
+
     public static void main(String[] args) throws IOException {
         Pattern pattern = Pattern.compile("\\PL+");
 
@@ -33,16 +40,13 @@ public class Program {
                 System.out.printf("\nWyrazy na litere %C: ", key);
                 value.stream().forEach(word -> System.out.printf("%s (%d) ", word.getKey(), word.getValue()));
             });
-        
-            //IntSummaryStatistics summary = pureTextList.stream().mapToInt(e -> Integer.parseInt(e)).summaryStatistics();
-            //System.out.println(summary.getCount());
 
             LongSummaryStatistics stats = wordCounts.entrySet().stream().mapToLong(x -> x.getValue()).summaryStatistics();
             LongSummaryStatistics stats2 = wordCounts.entrySet().stream().mapToLong(x -> x.getKey().length()).summaryStatistics();
             LongSummaryStatistics stats3 = wordCounts.entrySet().stream().mapToLong(x -> x.getKey().length()).distinct().summaryStatistics();
             DoubleSummaryStatistics avg = wordCounts.entrySet().stream().mapToDouble(x -> x.getValue() * x.getKey().length()).summaryStatistics();
             System.out.printf("\n\nLaczna liczba slow: %d\nLiczba roznych slow: %d\nMinimalna dlugosc slowa: %d\nMaksymalna dlugosc slowa: %d\nSrednia dlugosc slow: %f",
-                             stats.getSum(), stats3.getSum(), stats2.getMin(), stats2.getMax(), avg.getSum()/stats.getSum());
+                             stats.getSum(), stats.getCount(), stats2.getMin(), stats2.getMax(), avg.getSum()/stats.getSum());
         
         
             System.out.println("\n\nPierwsze 10 slow tekstu(+ dlugosc poszczegolnych slow): ");
@@ -63,10 +67,15 @@ public class Program {
                 });
 
             System.out.println("\nSuma(laczna liczba liter): ");
-            
-            //System.out.println(IntStream.range(pureTextList.stream()., 10).sum());
-            //pureTextList.stream().map(e -> e.replaceAll(", ", "")).takeWhile(e -> e.length() > 0);
 
             System.out.println(pureTextList.stream().mapToInt(String::length).sum());
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Prosze wprowadzic wzorzec do wyszukania w tekscie: (wielkosc liter nie ma znaczenia): ");
+            String userInput = scanner.next();
+            Optional<Long> occurence = check(pureTextList.stream().map(String::toUpperCase).filter(e -> e.contains(userInput.toUpperCase())).count());
+            occurence.ifPresentOrElse(e -> {System.out.printf("Fraza: %s wystepuje w tekscie: %d razy\nOto one: ", userInput, occurence.get());
+                pureTextList.stream().map(String::toUpperCase).filter(s -> s.contains(userInput.toUpperCase())).forEach(x -> System.out.print(x.toLowerCase() + " "));}, 
+                () -> System.out.printf("Fraza: %s nie znajduje sie w tekscie", userInput));
+            scanner.close();
     }    
 }
