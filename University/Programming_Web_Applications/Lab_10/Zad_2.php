7940988da702d3bdbd4036445dd27324759756bd
DeTry <sniacyinaczej@gmail.com>
2023-01-05 19:56:27 +0100

    add

--- University/Programming_Web_Applications/Lab_10/Zad_2.php
+++ University/Programming_Web_Applications/Lab_10/Zad_2.php
@@ -8,15 +8,33 @@
     <body>
         <?php
             function losuj_string() {
-                $i = rand() % 4;
+                $i = rand() % 3;
                 switch($i) {
                     case 0: return "kamień";
                     case 1: return "papier";
                     case 2: return "nożyce";
                 }
             }
-            $wybor = losuj_string();
-            echo("Wybór przeciwnika: ".$wybor.<br>);
         ?>
+            <form action="Zad_2.php">
+                <input name="imie">
+                <input name="nazwisko">
+                <button onclick="submit()">Prześlij</button>
+            </form>
+        <?php
+            
+            if(!empty($_GET)) {
+                $wybor = losuj_string();
+                echo("Użytkownik: ".$_GET["imie"]." ".$_GET["nazwisko"]." wylosował: ".$wybor."<br>");   
+            } else {
+                $user = "brak";
+                echo($user);
+            }
+            
+        ?>
+
+        <a href="Zad_2.php?jaki=kamień">Kamień</a>
+        <a href="Zad_2.php?jaki=papier">Papier</a>
+        <a href="Zad_2.php?jaki=nożyce">Nożyce</a>
     </body>
 </html>
