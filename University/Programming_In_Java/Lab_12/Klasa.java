5733431c6a5a3938b48bee76f84d40323cde445a
DeTry <sniacyinaczej@gmail.com>
2023-01-10 01:29:51 +0100

    new

---
+++ University/Programming_In_Java/Lab_12/Klasa.java
@@ -0,0 +1,60 @@
+import java.util.Random;
+
+public class Klasa {
+    private int surowiec[];
+    private int magazynSurowcow;
+    private int produktA;
+    private int produktB;
+    private int produktC;
+
+    public Klasa(int surowiec, final int zrodlo) {
+        this.surowiec = new int[zrodlo];
+        for (int i = 0; i < 2; ++i) {
+            this.surowiec[i] = surowiec; 
+        }
+        this.magazynSurowcow = 0;
+        produktA = 0;
+        produktB = 0;
+        produktC = 0;
+    }
+
+    public int getMagazynSurowcow() { return magazynSurowcow; }
+    public static String getProduct(int value) { 
+        switch(value) {
+            case 0: return "Produkt A"; 
+            case 1: return "Produkt B";
+            case 2: return "Produkt C";
+            default: return "Blad";
+        }
+    }
+    
+    public synchronized void procesProducenta() throws InterruptedException {
+        Random random = new Random();
+        int ktory = random.nextInt(3);
+        while (!(ktory == 0 && magazynSurowcow >= 120 && magazynSurowcow < 250) || 
+            !(ktory == 1 && magazynSurowcow >= 250 && magazynSurowcow < 500) ||
+            !(ktory == 2 && magazynSurowcow >= 500)) {
+            System.out.println(Thread.currentThread().getName() + 
+            ": Czekam na materialy, chce wyprodukowac: " + Klasa.getProduct(ktory));
+            wait();
+        }
+        System.out.println("DZIALA!");
+    }
+
+    public void transfer(int liczbaSurowcow, int zrodlo) {
+        magazynSurowcow += liczbaSurowcow;
+        surowiec[zrodlo] -= liczbaSurowcow;
+    }    
+
+    public void procesDostawcy() {
+        if (surowiec[0] != 0 && surowiec[1] != 0) {
+            Random random = new Random();
+            int liczbaSurowcow = random.nextInt(100, 300);
+            int zrodlo = random.nextInt(2);
+            transfer(liczbaSurowcow, zrodlo);
+            System.out.println(Thread.currentThread().getName() + 
+                ": Pobralem " + liczbaSurowcow + " jednostek ze zrodla: " + zrodlo + " (w magazynie pozostalo: " +
+                surowiec[zrodlo] + " szt.)");
+        }
+    }
+}
---
+++ University/Programming_In_Java/Lab_12/Program.java
@@ -0,0 +1,35 @@
+public class Program {
+    public static void main(String[] args) {
+        final int zrodlo = 2;
+        Klasa klasa = new Klasa(50000, zrodlo);
+
+        Runnable producent = () -> {
+            try {
+                klasa.procesProducenta();
+            } catch (InterruptedException e) {}
+        };
+
+        Runnable dostawca = () -> {
+            klasa.procesDostawcy();
+        };
+
+        Thread thread1 = new Thread(producent);
+        Thread thread2 = new Thread(producent);
+
+        Thread thread3 = new Thread(dostawca);
+        Thread thread4 = new Thread(dostawca);
+        Thread thread5 = new Thread(dostawca);
+
+        thread1.setName("Producent 1");
+        thread2.setName("Producent 2");
+
+        thread3.setName("Dostawca 1");
+        thread4.setName("Dostawca 2");
+        thread5.setName("Dostawca 3");
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+        
+    }
+}
